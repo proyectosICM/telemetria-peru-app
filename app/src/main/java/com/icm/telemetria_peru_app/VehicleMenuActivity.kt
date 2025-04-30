9f28ad60ab26afc0b429b121f0d83ace8a413532
Eduardo Aguilar <es123y123@gmail.com>
2025-04-30 17:52:38 -0500

    c

---
+++ app/src/main/java/com/icm/telemetria_peru_app/VehicleMenuActivity.kt
@@ -0,0 +1,11 @@
+package com.icm.telemetria_peru_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VehicleMenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vehicle_menu)
+    }
+}
